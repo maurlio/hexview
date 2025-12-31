d9ac10a3d39a4a32c5ebed3343ac17d25b85f4c3
Maurilio Felipe Rodrigues <maurilioofelipe@yahoo.com>
2025-12-31 14:27:44 -0300

    feat(main): adiciona saída hexadecimal com representação ASCII

--- src/main.c
+++ src/main.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 4096
+#define BYTES_PER_LINE 16
 
 /**
- * Tamanho do buffer de leitura.
- * 4096 bytes (4KB) alinhado com o tamanho de página de memória.
+ * Imprimi uma linha formatada.
  */
-#define BUFFER_SIZE 4096
+void print_hex_line(const uint8_t *buffer, size_t length, size_t offset)
+{
+    printf("%08zx: ", offset);
+
+    /* Bytes em Hexadecimal */
+    for (size_t i = 0; i < BYTES_PER_LINE; i++)
+    {
+        if (i < length)
+        {
+            printf("%02x ", buffer[i]);
+        }
+        else
+        {
+            printf("    ");
+        }
+    }
+
+    printf(" ");
+
+    /* Representação ASCII */
+    for (size_t i = 0; i < BYTES_PER_LINE; i++)
+    {
+        if (i < length)
+        {
+            unsigned char c = buffer[i];
+            printf("%c", isprint((int)c) ? c : '.');
+        }
+    }
+
+    printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -27,11 +60,22 @@ int main(int argc, char *argv[])
     }
 
     uint8_t buffer[BUFFER_SIZE];
-    size_t bytes_read, total_bytes = 0;
+    size_t bytes_read, global_offset = 0;
 
     while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp)) > 0)
     {
-        total_bytes += bytes_read;
+        for (size_t i = 0; i < bytes_read; i += BYTES_PER_LINE)
+        {
+            size_t line_length = BYTES_PER_LINE;
+            if (i + line_length > bytes_read)
+            {
+                line_length = bytes_read - i;
+            }
+
+            print_hex_line(buffer + i, line_length, global_offset + i);
+        }
+
+        global_offset += bytes_read;
     }
 
     if (ferror(fp))
@@ -40,8 +84,6 @@ int main(int argc, char *argv[])
         fclose(fp);
         return EXIT_FAILURE;
     }
-
-    printf("hexview: %zu bytes lidos de '%s' \n", total_bytes, file_name);
 
     fclose(fp);
     return EXIT_FAILURE;
